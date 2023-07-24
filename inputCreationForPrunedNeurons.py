8214e338052799ed00c8d8a966d152523669d21f
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-07-24 10:47:13 +0200

    fix py file for inputs

--- inputCreationForPrunedNeurons.py
+++ inputCreationForPrunedNeurons.py
@@ -7,14 +7,31 @@ if __name__ == "__main__":
     outputFolder = sys.argv[3]
 
     columns = [f'N{i:02d}' for i in range(100)]
-    dfInputs = pd.read_csv(allInputsFile, sep='', header=None, names=columns, index_col=False)
+    dfInputs = pd.DataFrame(columns=columns)
+    with open(allInputsFile, 'r') as f:
+        count = 0
+        while True:
+            line = f.readline()
+
+            if not line:
+                break
+
+            line = list(map(int, list(line)[:-1]))
+            dfInputs = pd.concat([dfInputs, pd.DataFrame(line, index=columns, columns=[0]).transpose()], axis=0)
+            count += 1
+            if count % 5000 == 0:
+                print(f'{count} lines read')
+                break
+        print(f'{allInputsFile} read')
 
     dfPrune = pd.read_csv(pruneFilename)
+    print(f'{pruneFilename} read')
 
     for neuron in dfPrune.columns:
         df = dfInputs.drop(dfInputs.columns[dfPrune[neuron]], axis=1)
 
         with open(f'{outputFolder}/{neuron}', 'w') as f:
             for index, row in df.iterrows():
-                f.write(''.join(row.to_string(header=False, index=False).split('\n')))
+                f.write(''.join(row.to_string(header=False, index=False).replace('\n', '')))
                 f.write('\n')
+            print(f'{outputFolder}/{neuron} created successfully')
