765c0f360835d6c9aa22acb5cc23e4f4721793fb
CJLlorente99 <carlosllo@hotmail.com>
2023-07-25 16:04:50 +0200

    bug fixing

--- espressoShGeneration.py
+++ espressoShGeneration.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
 
     for dir in dirs:
         with open('espressoExe.sh', 'w') as f:
-            f.write(f'ESPRESSOing {inputPLAFolder}/{dir.name}\n')
-            f.write(f'./espresso -efast {inputPLAFolder}/{dir.name} > {outputPLAFolder}/{dir.name}.pla\n')
-            f.write(f'ESPRESSOed {inputPLAFolder}/{dir.name}\n')
+            f.write(f'printf "ESPRESSOing {inputPLAFolder}/{dir.name}"\n')
+            f.write(f'../espresso-logic/bin/espresso -efast -o fr {inputPLAFolder}/{dir.name} > {outputPLAFolder}/{dir.name}.pla\n')
+            f.write(f'printf "ESPRESSOed {inputPLAFolder}/{dir.name}"\n')
         f.close()
+    os.chmod("espressoExe.sh", 0o777)
+	
