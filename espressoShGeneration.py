a8c6842aede2d6ef0abb05b2dd2adc344bc7873d
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-07-27 11:17:30 +0200

    .

--- espressoShGeneration.py
+++ espressoShGeneration.py
@@ -8,13 +8,13 @@ if __name__ == "__main__":
     if not os.path.exists(outputPLAFolder):
         os.makedirs(outputPLAFolder)
 
-    dirs = os.scandir(inputPLAFolder)
+    files = os.scandir(inputPLAFolder)
 
-    for dir in dirs:
-        with open('espressoExe.sh', 'w') as f:
-            f.write(f'printf "ESPRESSOing {inputPLAFolder}/{dir.name}"\n')
-            f.write(f'../espresso-logic/bin/espresso -efast -o fr {inputPLAFolder}/{dir.name} > {outputPLAFolder}/{dir.name}.pla\n')
-            f.write(f'printf "ESPRESSOed {inputPLAFolder}/{dir.name}"\n')
-        f.close()
+    with open('espressoExe.sh', 'w') as f:
+        for file in files:
+            f.write(f'printf "ESPRESSOing {inputPLAFolder}/{dir.name}\n"\n')
+            f.write(f'../espresso-logic/bin/espresso -efast -o fr {inputPLAFolder}/{file.name} > {outputPLAFolder}/{file.name}.pla\n')
+            f.write(f'printf "ESPRESSOed {inputPLAFolder}/{file.name}\n"\n\n')
+    f.close()
     os.chmod("espressoExe.sh", 0o777)
 	
