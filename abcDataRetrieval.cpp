acd88cf2a64fb650733648a90d932fa850fb54c6
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-08-11 10:36:49 +0200

    .

--- abcDataRetrieval.cpp
+++ abcDataRetrieval.cpp
@@ -27,8 +27,8 @@ int main(int argc, char** argv){
     vector<string> aigerSubfolders;
     for(const auto& entry : std::experimental::filesystem::directory_iterator(modelAigerFolder)){
         if(std::experimental::filesystem::is_directory(entry)){
-            printf("%s\n", entry.path().string());
-            aigerSubfolders.push_back(entry.path().string());
+            printf("%s\n", entry.path());
+            aigerSubfolders.push_back(entry.path());
         }
     }
 
