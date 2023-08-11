898adc52516c25c81cfbd8fae74cf2ceefa1a8d8
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-08-11 12:09:02 +0200

    .

--- abcDataRetrieval.cpp
+++ abcDataRetrieval.cpp
@@ -21,53 +21,63 @@ using namespace std;
 
 int main(int argc, char** argv){
     /* Parse arguments */
-    string modelAigerFolder = argv[1];
-
+    string modelFolder = argv[1];
     /* List all the Subfolders files in the folder */
-    vector<string> aigerSubfolders;
-    for(const auto& entry : std::experimental::filesystem::directory_iterator(modelAigerFolder)){
+    vector<string> subfolders;
+    for(const auto& entry : std::experimental::filesystem::directory_iterator(modelFolder)){
         if(std::experimental::filesystem::is_directory(entry)){
-            aigerSubfolders.push_back(entry.path().string());
+            subfolders.push_back(entry.path().string());
         }
     }
 
-    /* ABC Interaction */
-    Abc_Frame_t* pAbc;
-    char command[1024];
+    for(const auto& modelAigerFolder : subfolders){
 
-    // Start framework
-    Abc_Start();
-    pAbc = Abc_FrameGetGlobalFrame();
-
-    // Iterate through all the PLA files
-    int i = 0;
-    for(const auto& aigFolder : aigerSubfolders){
-        /* List all the AIGER files in the folder */
-        vector<string> aigerFiles;
-        for(const auto& entry : std::experimental::filesystem::directory_iterator(aigFolder)){
-            if(std::experimental::filesystem::is_regular_file(entry)){
-                aigerFiles.push_back(entry.path().string());
+        /* List all the Subfolders files in the folder */
+        vector<string> aigerSubfolders;
+        for(const auto& entry : std::experimental::filesystem::directory_iterator(modelAigerFolder)){
+            if(std::experimental::filesystem::is_directory(entry)){
+                aigerSubfolders.push_back(entry.path().string());
             }
         }
 
-        vector<string> results;
-        for (const auto& aigFile : aigerFiles){
-            const char* filename = aigFile.c_str();
+        /* ABC Interaction */
+        Abc_Frame_t* pAbc;
+        char command[1024];
 
-            // Read the AIG file
-            sprintf(command, "read_aiger %s", filename);
-            // cout << command << endl;
-            if ( Cmd_CommandExecute(pAbc, command) ) {
-            fprintf(stdout, "Error reading AIGER file %s\n", command);
-            return 1;
+        // Start framework
+        Abc_Start();
+        pAbc = Abc_FrameGetGlobalFrame();
+
+        // Iterate through all the PLA files
+        int i = 0;
+        for(const auto& aigFolder : aigerSubfolders){
+            /* List all the AIGER files in the folder */
+            vector<string> aigerFiles;
+            for(const auto& entry : std::experimental::filesystem::directory_iterator(aigFolder)){
+                if(std::experimental::filesystem::is_regular_file(entry)){
+                    aigerFiles.push_back(entry.path().string());
+                }
             }
 
-            // Print stats
-            sprintf(command, "print_stats");
-            // cout << command << endl;
-            if ( Cmd_CommandExecute(pAbc, command) ) {
-                fprintf(stdout, "Error printintg stats %s\n", command);
+            vector<string> results;
+            for (const auto& aigFile : aigerFiles){
+                const char* filename = aigFile.c_str();
+
+                // Read the AIG file
+                sprintf(command, "read_aiger %s", filename);
+                // cout << command << endl;
+                if ( Cmd_CommandExecute(pAbc, command) ) {
+                fprintf(stdout, "Error reading AIGER file %s\n", command);
                 return 1;
+                }
+
+                // Print stats
+                sprintf(command, "print_stats");
+                // cout << command << endl;
+                if ( Cmd_CommandExecute(pAbc, command) ) {
+                    fprintf(stdout, "Error printintg stats %s\n", command);
+                    return 1;
+                }
             }
         }
     }
