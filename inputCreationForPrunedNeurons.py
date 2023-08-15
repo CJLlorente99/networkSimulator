96fb8fe91ca22c1cf8aa9219b5d7155a98844ec3
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-08-15 08:39:01 +0200

    chunks for np txt too

--- inputCreationForPrunedNeurons.py
+++ inputCreationForPrunedNeurons.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 import os
+from itertools import islice
 
 if __name__ == "__main__":
     allInputsFile = sys.argv[1]
@@ -40,28 +41,40 @@ if __name__ == "__main__":
                 chunkNum += 1
             print(f'Chunk [{chunkNum}] [{count:4d}/{totalNeurons:04d}] created successfully')
     else:
-        dfInputs = np.genfromtxt(allInputsFile, delimiter=1)
-        columns = [f'N{i:04d}' for i in range(dfInputs.shape[1])]
-        dfInputs = pd.DataFrame(dfInputs, columns=columns)
-        dfInputs.astype(int)
-        print(f'{allInputsFile} read. Shape {dfInputs.shape}')
+        with open(allInputsFile, 'r') as f:
+            chunkNum = 0
+            count = 0
+            while True:
+                gen = islice(f, 10000)
+                dfInputs = np.genfromtxt(allInputsFile, delimiter=1)
+                
+                if dfInputs.shape[0] == 0:
+                    break
 
-        dfPrune = pd.read_csv(pruneFilename)
-        print(f'{pruneFilename} read')
-        
-        totalNeurons = len(dfPrune.columns)
-        count = 0
-        for neuron in dfPrune.columns:
-            df = dfInputs.drop(dfInputs.columns[dfPrune[neuron]], axis=1)
-            dfNp = df.to_numpy()
+                columns = [f'N{i:04d}' for i in range(dfInputs.shape[1])]
+                dfInputs = pd.DataFrame(dfInputs, columns=columns)
+                dfInputs.astype(int)
+                print(f'{allInputsFile} read. Shape {dfInputs.shape}')
 
-            np.savetxt(f'{outputFolder}/{neuron}',
-                        dfNp,
-                        delimiter='',
-                        fmt='%i',
-                        newline='\n')
-            if (count % 100) == 0:
-                print(f'[{count:4d}/{totalNeurons:04d}] created successfully')
-            count += 1
-        print(f'[{count:4d}/{totalNeurons:04d}] created successfully')
+                dfPrune = pd.read_csv(pruneFilename)
+                print(f'{pruneFilename} read')
+                
+                totalNeurons = len(dfPrune.columns)
+                count = 0
+                for neuron in dfPrune.columns:
+                    df = dfInputs.drop(dfInputs.columns[dfPrune[neuron]], axis=1)
+                    dfNp = df.to_numpy()
+
+                    with open(f'{outputFolder}/{neuron}', 'a') as f:
+                        np.savetxt(f,
+                                dfNp,
+                                delimiter='',
+                                fmt='%i',
+                                newline='\n')
+                    f.close()
+                    if (count % 100) == 0:
+                        print(f'Chunk [{chunkNum}] [{count:4d}/{totalNeurons:04d}] created successfully')
+                    count += 1
+                    chunkNum += 1
+                print(f'Chunk [{chunkNum}] [{count:4d}/{totalNeurons:04d}] created successfully')
 
